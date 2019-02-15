4a2a1c3823b7c2334718f70cafbb6fb92fec7438
Zachary Tanverakul <zata6314@colorado.edu>
2019-02-14 18:17:10 -0700

    Create Shapes.py

---
+++ Shapes.py
@@ -0,0 +1,34 @@
+class Shape:
+  def __init__(self, database):
+    self.database = database
+
+  #def SortShape(self):
+
+  def DisplayShape(self):
+    for i in range(0, len(self.database)):
+      print("the shape is a", self.database[i])
+  
+  def DatabaseSize(self):
+    print("The number of shapes in the database is", len(self.database))
+
+class Circle(Shape):
+  def getname():
+    return('Circle')
+
+class Triangle(Shape):
+  def getname(): 
+    return('Triangle')
+
+class Square(Shape):
+  def getname():
+    return('Square') 
+
+database = []
+
+database.append(Circle.getname())
+database.append(Triangle.getname())
+database.append(Square.getname())
+
+currentinst = Shape(database)
+currentinst.DatabaseSize()
+currentinst.DisplayShape()
